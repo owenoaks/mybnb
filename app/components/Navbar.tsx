607f535c94688559c2eb1690eb59c9b452a2e853
“Ewin” <ewinoaks@gmail.com>
2024-04-04 11:01:48 +0800

    static navbar done

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+import DesktopLogo from "../../public/airbnb-desktop.png";
+import MobileLogo from "../../public/airbnb-mobile.webp";
+import UserNav from "./UserNav";
+//CONTINUE 21:28
+//import { SearchModalCompnent } from "./SearchComponent";
+
+export function Navbar() {
+  return (
+    <nav className="w-full border-b">
+      <div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-5">
+        <Link href="/">
+          <Image
+            src={DesktopLogo}
+            alt="Desktop Logo"
+            className="w-32 hidden lg:block"
+          />
+
+          <Image
+            src={MobileLogo}
+            alt="Mobile Logo"
+            className="block lg:hidden w-12"
+          />
+        </Link>
+        <div className="rounded-full border px-5 py-2">
+          <h1>Hello from Search</h1>
+        </div>
+        <UserNav />
+      </div>
+    </nav>
+  );
+}
---
+++ app/components/UserNav.tsx
@@ -0,0 +1,29 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { MenuIcon } from "lucide-react";
+
+export default function UserNav() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <div className="rounded-full border px-2 py-2 lg:px-4 lg:py-2 flex items-center gap-x-3">
+          <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5" />
+          <img
+            src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
+            alt="user image"
+            className="rounded-full h-8 w-8 hidden lg:block"
+          />
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-[200px]">
+        <DropdownMenuItem>Register</DropdownMenuItem>
+        <DropdownMenuItem>Login</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
